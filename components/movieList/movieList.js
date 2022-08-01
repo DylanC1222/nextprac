2be364d7d7de52d5b24904a5f4a62444f356a7df
DylanC1222 <77405374+DylanC1222@users.noreply.github.com>
2022-08-01 23:17:03 +0900

    animation cool for movieList

--- components/movieList/movieList.js
+++ components/movieList/movieList.js
@@ -1,34 +1,104 @@
-import Carousel from "react-bootstrap/Carousel";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
+import Card from "react-bootstrap/Card";
+import { useState } from "react";
+
+// Create a vertical choose area for which movie to check ranking of
 
 export default function MovieList({ results }) {
+  const [open, setOpen] = useState([
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
   return (
     <>
-      <div
-        className="movie-container"
-        style={{
-          boxSizing: "border-box",
-          display: "block",
-          margin: "0 0 4rem",
-        }}
-      >
-        <Carousel>
-          {results.map((item, idx) => {
+      <div className="movie-container d-flex flex-row justify-content-center">
+        <div className="movie-rank-cards">
+          {Array.from({ length: 10 }).map((_, idx) => {
             return (
-              <Carousel.Item key={idx}>
-                <img
-                  className="d-block w-100"
-                  src={"http://localhost:3000/images/movie_placeholder.jpg"}
-                />
-                <Carousel.Caption>
-                  <h2>{item.title}</h2>
-                  <div className="score-block">
-                    <h2 className="score">{item.vote_average}</h2>
-                  </div>
-                </Carousel.Caption>
-              </Carousel.Item>
+              <Collapse
+                in={open[idx]}
+                dimension="width"
+                style={{ transition: "0.3s ease-in-out" }}
+              >
+                <div className="movie-card">
+                  <Card
+                    className={"d-flex flex-row border-0"}
+                    style={{ width: "100%" }}
+                  >
+                    <Card.Body
+                      style={{
+                        maxWidth: "200px",
+                        minWidth: "200px",
+                        height: "100px",
+                      }}
+                    >
+                      <Card.Title style={{ fontSize: "150%" }}>
+                        {results[idx].title}
+                      </Card.Title>
+                      <Card.Text className="text-dark">
+                        {results[idx].vote_average}
+                      </Card.Text>
+                    </Card.Body>
+                    <Card.Img
+                      style={{
+                        maxWidth: "800px",
+                        minWidth: "800px",
+                        height: "",
+                        borderRadius: "0",
+                      }}
+                      src={"http://localhost:3000/images/movie_placeholder.jpg"}
+                    />
+                  </Card>
+                </div>
+              </Collapse>
             );
           })}
-        </Carousel>
+        </div>
+        <div className="movie-rank-buttons">
+          <ButtonGroup vertical style={{ height: "450px" }}>
+            {Array.from({ length: 10 }).map((_, idx) => {
+              return (
+                <Button
+                  style={{ borderRadius: "0" }}
+                  onClick={() => {
+                    setTimeout(() => {
+                      setOpen(
+                        open.map((_, innerIdx) =>
+                          innerIdx === idx ? true : false
+                        )
+                      );
+                    }, 300);
+                    setOpen([
+                      false,
+                      false,
+                      false,
+                      false,
+                      false,
+                      false,
+                      false,
+                      false,
+                      false,
+                      false,
+                    ]);
+                  }}
+                >
+                  {idx + 1}
+                </Button>
+              );
+            })}
+          </ButtonGroup>
+        </div>
       </div>
     </>
   );
